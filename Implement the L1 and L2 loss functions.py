ecc158dc558c97a4f1b502a3e883ea99a40071d3
hu <tubao9hao@126.com>
2018-12-22 21:34:18 +0800

    1

---
+++ Implement the L1 and L2 loss functions.py
@@ -0,0 +1,26 @@
+"""
+Exercise: Implement the numpy vectorized version of the L1 loss. You may find the function abs(x) (absolute value of x) useful.
+"""
+
+
+import numpy as np
+
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+
+    loss = np.sum(np.abs(y - yhat))
+    return loss
+
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L1 = " + str(L1(yhat,y)))
+
+print(0.1+0.1+0.1 == 0.3)
