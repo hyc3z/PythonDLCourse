510341feee356a80d98f80321dfc3a6166f48504
hu <tubao9hao@126.com>
2018-12-23 18:35:38 +0800

    1

--- Logistic Regression with a Neural Network mindset.py
+++ Logistic Regression with a Neural Network mindset.py
@@ -6,3 +6,8 @@ from PIL import Image
 from scipy import ndimage
 from lr_utils import load_dataset
 
+
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+index = 25
+plt.imshow(train_set_x_orig[index])
+print ("y = " + str(train_set_y[:, index]) + ", it's a '" +classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
