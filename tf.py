6d3627d727d8f5921941a5356caba8fe989a6a95
hu <tubao9hao@126.com>
2019-02-26 21:12:56 +0800

    1902262112

--- tf.py
+++ tf.py
@@ -2,27 +2,36 @@ import tensorflow as tf
 import numpy as np
 
 
-x_data = np.random.rand(100).astype(np.float32)
-y_data = x_data*0.1 + 0.3
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
 
-Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-biases = tf.Variable(tf.zeros([1]))
 
-y = Weights*x_data + biases
+x_data = np.linspace(-1,1,300)[:,np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
 
-loss = tf.reduce_mean(tf.square(y-y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.5)
-train = optimizer.minimize(loss)
+xs = tf.placeholder(tf.float32,[None,1])
+ys = tf.placeholder(tf.float32,[None,1])
 
+l1 = add_layer(xs, 1, 10, tf.nn.relu)
+l2 = add_layer(l1, 10, 1)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(y_data - l2),reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 init = tf.global_variables_initializer()
-
 sess = tf.Session()
 sess.run(init)
 
 
-for _ in range(201):
-    sess.run(train)
-    if not _%20 :
-        print(_, sess.run(Weights), sess.run(biases))
+for i in range(2000):
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if not i%50 :
+        print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
 
 file_writer = tf.summary.FileWriter('./logs', sess.graph)
