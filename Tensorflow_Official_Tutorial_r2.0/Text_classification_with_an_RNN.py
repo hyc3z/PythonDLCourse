3d85e932057ff5884a44e1c81d20ae71695f9b34
hu <tubao9hao@126.com>
2019-04-02 19:36:01 +0800

    Add Transfer_learning and RNN

---
+++ Tensorflow_Official_Tutorial_r2.0/Text_classification_with_an_RNN.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow_datasets as tfds
+import tensorflow as tf
+
+import matplotlib.pyplot as plt
+
+
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+
+
+dataset, info = tfds.load('imdb_reviews/subwords8k', with_info=True,
+                          as_supervised=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
+
+tokenizer = info.features['text'].encoder
+
+print ('Vocabulary size: {}'.format(tokenizer.vocab_size))
+
+sample_string = 'TensorFlow is cool.'
+
+tokenized_string = tokenizer.encode(sample_string)
+print ('Tokenized string is {}'.format(tokenized_string))
+
+original_string = tokenizer.decode(tokenized_string)
+print ('The original string: {}'.format(original_string))
+
+assert original_string == sample_string
+
+for ts in tokenized_string:
+    print ('{} ----> {}'.format(ts, tokenizer.decode([ts])))
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 64
+
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.padded_batch(BATCH_SIZE, train_dataset.output_shapes)
+test_dataset = test_dataset.padded_batch(BATCH_SIZE, test_dataset.output_shapes)
+
+model = tf.keras.Sequential([
+    # Params: 8185*64 = 523840
+    tf.keras.layers.Embedding(tokenizer.vocab_size, 64),
+
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.summary()
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
---
+++ Tensorflow_Official_Tutorial_r2.0/Transfer_learning.py
@@ -0,0 +1,116 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import tensorflow as tf
+from tensorflow.python import keras
+print("TensorFlow version is ", tf.__version__)
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+base_dir = '/home/hu/tensorflow_datasets/cats_vs_dogs/PetImages'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+print(train_dir)
+# Directory with our training cat pictures
+train_cats_dir = os.path.join(train_dir, 'cats')
+print ('Total training cat images:', len(os.listdir(train_cats_dir)))
+
+# Directory with our training dog pictures
+train_dogs_dir = os.path.join(train_dir, 'dogs')
+print ('Total training dog images:', len(os.listdir(train_dogs_dir)))
+
+# Directory with our validation cat pictures
+validation_cats_dir = os.path.join(validation_dir, 'cats')
+print ('Total validation cat images:', len(os.listdir(validation_cats_dir)))
+
+# Directory with our validation dog pictures
+validation_dogs_dir = os.path.join(validation_dir, 'dogs')
+print ('Total validation dog images:', len(os.listdir(validation_dogs_dir)))
+
+image_size = 160 # All images will be resized to 160x160
+batch_size = 32
+
+# Rescale all images by 1./255 and apply image augmentation
+train_datagen = keras.preprocessing.image.ImageDataGenerator(
+                rescale=1./255)
+
+validation_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+# Flow training images in batches of 20 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+                train_dir,  # Source directory for the training images
+                target_size=(image_size, image_size),
+                batch_size=batch_size,
+                # Since we use binary_crossentropy loss, we need binary labels
+                class_mode='binary')
+
+# Flow validation images in batches of 20 using test_datagen generator
+validation_generator = validation_datagen.flow_from_directory(
+                validation_dir, # Source directory for the validation images
+                target_size=(image_size, image_size),
+                batch_size=batch_size,
+                class_mode='binary')
+
+IMG_SHAPE = (image_size, image_size, 3)
+
+# Create the base model from the pre-trained model MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+base_model.trainable = False
+base_model.summary()
+
+model = tf.keras.Sequential([
+  base_model,
+  keras.layers.GlobalAveragePooling2D(),
+  keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=0.0001),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+print(len(model.trainable_variables))
+
+epochs = 10
+steps_per_epoch = train_generator.n // batch_size
+validation_steps = validation_generator.n // batch_size
+
+history = model.fit_generator(train_generator,
+                              steps_per_epoch=steps_per_epoch,
+                              epochs=epochs,
+                              workers=4,
+                              validation_data=validation_generator,
+                              validation_steps=validation_steps)
+
+model.save('./Transfer_learning.h5')
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,max(plt.ylim())])
+plt.title('Training and Validation Loss')
+plt.show()
