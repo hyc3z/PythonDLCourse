3f9ab206cdbf220370dac654671f5af5378f923f
hu <tubao9hao@126.com>
2019-04-13 07:41:13 +0800

    Add per_epoch save model support

--- Experimental/Test_multiuser.py
+++ Experimental/Test_multiuser.py
@@ -2,7 +2,7 @@ from tensorflow.python import keras
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
 from tensorflow.python.keras.layers.normalization import BatchNormalization
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import os
 import tensorflow as tf
@@ -161,20 +161,20 @@ val_acc = history.history['val_acc']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(acc, label='Training Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.ylabel('Accuracy')
-plt.ylim([min(plt.ylim()),1])
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(2, 1, 2)
-plt.plot(loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.ylabel('Cross Entropy')
-plt.ylim([0,max(plt.ylim())])
-plt.title('Training and Validation Loss')
-plt.show()
+# plt.figure(figsize=(8, 8))
+# plt.subplot(2, 1, 1)
+# plt.plot(acc, label='Training Accuracy')
+# plt.plot(val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.ylabel('Accuracy')
+# plt.ylim([min(plt.ylim()),1])
+# plt.title('Training and Validation Accuracy')
+#
+# plt.subplot(2, 1, 2)
+# plt.plot(loss, label='Training Loss')
+# plt.plot(val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.ylabel('Cross Entropy')
+# plt.ylim([0,max(plt.ylim())])
+# plt.title('Training and Validation Loss')
+# plt.show()
